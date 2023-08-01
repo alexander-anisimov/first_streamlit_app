d2798d4220dfd3bad6ca5fbf597ad01b5fa738ba
Alexander Anisimov <aliaksandr.anisimau@outlook.com>
2023-08-01 10:41:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 # a code will be added later
 import streamlit
 
-streamlit.title('Testing')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
